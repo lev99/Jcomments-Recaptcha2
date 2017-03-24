edc5a2a80ab3985e40a28a13a4264cbddfaa2623
Leonidas <leonidas78@mail.ru>
2017-03-24 19:34:36 +0300

    Update tpl_index.php
    Error: The align attribute on the div element is obsolete. Use CSS instead

--- site/tpl/default/tpl_index.php
+++ site/tpl/default/tpl_index.php
@@ -74,7 +74,7 @@ jcomments.setList('comments-list');
 				}
 			}
 ?>
-<div id="comments-footer" align="center"><?php echo $this->getVar('support'); ?></div>
+<div id="comments-footer" style="margin-left: auto; margin-right: auto;"><?php echo $this->getVar('support'); ?></div>
 <?php
 			// Some magic like dynamic comments list loader (anticache) and auto go to anchor script
 			$aca = (int) ($this->getVar('comments-gotocomment') == 1);
